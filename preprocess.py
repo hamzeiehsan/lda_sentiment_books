87e74389445c7b2ef02fe647852d1dddce0feefa
Ehsan Hamzei <ehsan.hamzei@unimelb.edu.au>
2023-06-07 23:40:17 +1000

    add spacy pos filter for tokenization

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,13 @@
 from collections import defaultdict
-from gensim import corpora
 import re
+import en_core_web_md
 
 
 RE_COMBINE_WHITESPACE = re.compile(r"\s+")
 
 
 class Processor:
-    def __init__(self, corpus):
+    def __init__(self, corpus, simple_tokenizer=True):
         self.raw = corpus
         self.corpus = []
         self.pre_tokenize()
@@ -15,8 +15,11 @@ class Processor:
         self.custom_stopwords = []
 
         self.wfrequencies = defaultdict(int)
-        self.tokens = [[word for word in document.lower().split() if word not in self.custom_stopwords]
-                       for document in self.corpus]
+        if simple_tokenizer:
+            self.tokens = [[word for word in document.lower().split() if word not in self.custom_stopwords]
+                        for document in self.corpus]
+        else:
+            self.spacy_tokenizer()
         self.count_word_frequencies()
 
     def add_stop_words(self, stop_words_list):
@@ -27,6 +30,16 @@ class Processor:
             tmp = text.translate({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+1234567890"})
             tmp = RE_COMBINE_WHITESPACE.sub(" ", tmp)
             self.corpus.append(tmp)
+
+    def spacy_tokenizer(self):
+        nlp = en_core_web_md.load()
+        # POS tags to remove
+        removal = ['ADV', 'PRON', 'CCONJ', 'PUNCT', 'PART', 'DET', 'ADP', 'SPACE', 'NUM', 'SYM']
+        self.tokens = []
+        for summary in nlp.pipe(self.corpus):
+            proj_tok = [token.lemma_.lower() for token in summary if
+                        token.pos_ not in removal and not token.is_stop and token.is_alpha]
+            self.tokens.append(proj_tok)
 
 
     def preprocess(self, force=False, min_freq=5, max_freq=200):
