a6b366a1dd9a6d75f8dd9d5f6e07da88ff70dcfb
Ehsan Hamzei <ehsan.hamzei@unimelb.edu.au>
2023-06-10 21:50:32 +1000

    Refactor vis, add log info, and refactor lda notebook

--- vis.py
+++ vis.py
@@ -1,3 +1,5 @@
+import logging
+
 import numpy as np
 import matplotlib
 import seaborn as sns
@@ -6,10 +8,12 @@ import pyLDAvis.gensim_models
 import pyLDAvis
 
 class Vis:
-    def __init__(self, df, notebook=False):
+    def __init__(self, sentiment, lda, notebook=False):
         if notebook:
             pyLDAvis.enable_notebook()  # Visualise inside a notebook
-        self.df = df
+        self.sentiment = sentiment
+        self.lda = lda
+        self.df = sentiment.df
         self.colorset_treemap = [
             "#fd7f6f", "#7eb0d5", "#b2e061", "#bd7ebe", "#ffb55a", "#ffee65", "#beb9db", "#fdcce5", "#8bd3c7",
             "#e60049", "#0bb4ff", "#50e991", "#e6d800", "#9b19f5", "#ffa300", "#dc0ab4", "#b3d4ff", "#00bfa0"
@@ -29,8 +33,9 @@ class Vis:
                                              'NEGATIVE': "#fd7f6f"})
         fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
         fig.write_html(file_name)
+        logging.info('chapter-sentiment treemap html file is saved in {}'.format(file_name))
 
-    def treemap_chapter_theme(self, file_name='figures/chapter_topic.html'):
+    def treemap_chapter_topic(self, file_name='figures/chapter_topic.html'):
         df_tmp = self.df. \
             groupby(['book', 'chapter', 'dominant_topic'], as_index=False). \
             agg({'paragraph_number': ['count']})
@@ -49,6 +54,7 @@ class Vis:
                          color_discrete_map=color_map)
         fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
         fig.write_html(file_name)
+        logging.info('chapter-topic treemap html is saved in: \n\t{}'.format(file_name))
 
     def treemap_topic_sentiment(self, file_name='figures/topic_sentiment.html'):
         df_tmp = self.df. \
@@ -67,3 +73,11 @@ class Vis:
                                              'NEGATIVE': "#fd7f6f"})
         fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
         fig.write_html(file_name)
+        logging.info('topic-sentiment treemap html is saved in: \n\t{}'.format(file_name))
+
+    def pyviz_topics(self, file_name='figures/pyvis_topics.html'):
+        lda_display = pyLDAvis.gensim_models.prepare(self.lda_model,
+                                                     self.lda.bow_corpus,
+                                                     self.lda.dictionary)
+        pyLDAvis.save_html(lda_display, file_name)
+        logging.info('pyvis html visualization is saved in: \n\t{}'.format(file_name))
