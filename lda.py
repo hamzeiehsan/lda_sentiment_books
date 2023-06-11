c2bc4823a231fe279be94706c47a63010d6b530b
Ehsan Hamzei <ehsan.hamzei@unimelb.edu.au>
2023-06-11 15:28:35 +1000

    Add HDP model

--- lda.py
+++ lda.py
@@ -1,11 +1,12 @@
 from gensim.corpora.dictionary import Dictionary
-from gensim.models import LdaMulticore
+from gensim.models import LdaMulticore, HdpModel
 from gensim.models import CoherenceModel
 from preprocess import Processor
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 
+
+import logging
 
 class LDAModel:
     def __init__(self, df):
@@ -16,20 +17,24 @@ class LDAModel:
         self.processed = []
         self.bow_corpus = []
         self.lda_model = None
+        self.hdp_model = None
         self.corpus_topics = []
 
     def preprocess(self, simple_tokenizer=True, custom_stopwords=[]):
+        logging.info("Calling preprocessing in LDA model:\n")
         self.processor = Processor(self.df, simple_tokenizer)
         # here, I can run any fancy preprocessing workflow that I need :)
         self.processor.preprocess()
         self.processed = self.processor.processed
         self.processor.map_to_df()
         self.df = self.processor.df
+        logging.info("Preprocessing is done - from LDA model:\n")
 
     def create_bow_model(self, filter_below=5, filter_above=200, keep_n=4000):
         self.dictionary = Dictionary(self.processed)
         self.dictionary.filter_extremes(no_below=filter_below, no_above=filter_above, keep_n=keep_n)
         self.bow_corpus = [self.dictionary.doc2bow(doc) for doc in self.processed]
+        logging.info("Bag of word representations are constructed...")
 
     def plot_optimal_coherent_scores(self, scoring_method="c_v", start_range=1, end_range=15):
         topics = []
@@ -69,17 +74,31 @@ class LDAModel:
         lda_model.print_topics(-1)
         self.lda_model = lda_model
 
-    def format_topics_sentences(self):
+    def create_hdp_model(self):
+        hdp_model = HdpModel(corpus=self.bow_corpus,
+                             id2word=self.dictionary,)
+        hdp_model.print_topics(-1)
+        self.hdp_model = hdp_model
+
+
+    def format_topics_sentences(self, use_lda_model=True):
+        model = None
+        if use_lda_model:
+            model = self.lda_model
+            logging.info("using LDA model to label topics for paragraphs")
+        else:
+            model = self.hdp_model
+            logging.info("using HDP model to label topics for paragraphs")
         # empty data frame
         sent_topics_df = pd.DataFrame()
         # retrieve main topic for each document
-        for i, row_list in enumerate(self.lda_model[self.bow_corpus]):
-            row = row_list[0] if self.lda_model.per_word_topics else row_list
+        for i, row_list in enumerate(model[self.bow_corpus]):
+            row = row_list[0] if model.per_word_topics else row_list
             row = sorted(row, key=lambda x: (x[1]), reverse=True)
             # Get the dominant topic, percentage of contribution and the set of keywords for each document
             for j, (topic_num, prop_topic) in enumerate(row):
                 if j == 0:  # => dominant topic
-                    wp = self.lda_model.show_topic(topic_num)
+                    wp = model.show_topic(topic_num)
                     topic_keywords = ", ".join([word for word, prop in wp])
                     sent_topics_df = pd.concat([sent_topics_df, pd.DataFrame(
                         [pd.Series([int(topic_num), round(prop_topic, 4), topic_keywords])])], ignore_index=True)
@@ -88,3 +107,4 @@ class LDAModel:
         sent_topics_df.columns = ['dominant_topic', 'perc_contribution', 'topic_keywords']
 
         self.df = pd.concat([self.df, sent_topics_df], axis=1)
+        logging.info("tagging topics are done, check the dataframe in LDAModel object\n")
